615228078f8232d0ea44d8aa738ad51e1b8c25ca
Keith Vassallo <keith@icemalta.com>
2023-08-30 15:42:27 +0100

    Module 1, Unit 5 complete.

---
+++ Module 1/Unit5/sc-bed-profilepage-starter/src/lib/bio.mjs
@@ -0,0 +1,6 @@
+export const bio = `<p>Hi! I'm Keith. I'm an Educator in coding<br> &amp; technical subjects.
+I'm skilled in<br> programming (Java, Python, PHP, C#,<br> JavaScript...).
+I'm also handy with Linux, <br>being certified by the Linux Professional<br> Institute, 
+and also cloud technology,<br> certified as an AWS Architect and<br> Developer.</p>
+<p><a href="mailto:keith@icemalta.com">
+    Let's chat</a> 💬</a></p>`;
